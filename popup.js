fb65b533d0ae492f2f64df2292d9d5d8c7a4428b
AsishRaju <41658324+AsishRaju@users.noreply.github.com>
2020-10-03 12:58:01 +0530

    Added Keyboard support ⌨

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ let equalsBtn = document.querySelector(".equal-btn");
 let cancelBtn = document.querySelector(".c-btn");
 let deleteBtn = document.querySelector(".del-btn");
 let displayValue = "";
+// let audio = new Audio('click.wav');
 
 document.querySelectorAll('.inp-btn').forEach(item => {
     item.addEventListener('click', event => {
@@ -9,11 +10,52 @@ document.querySelectorAll('.inp-btn').forEach(item => {
     });
 });
 
+document.addEventListener('keydown',(key)=>{
+    if (key.code === "Digit0" || key.code === "Numpad0") {
+        selectInput("0");
+    } else if (key.code === "Digit1" || key.code === "Numpad1") {
+        selectInput("1");
+    } else if (key.code === "Digit2"|| key.code === "Numpad2") {
+        selectInput("2");
+    } else if (key.code === "Digit3"|| key.code === "Numpad3") {
+        selectInput("3");
+    } else if (key.code === "Digit4"|| key.code === "Numpad4") {
+        selectInput("4");
+    } else if (key.code === "Digit5"|| key.code === "Numpad5") {
+        selectInput("5");
+    } else if (key.code === "Digit6"|| key.code === "Numpad6") {
+        selectInput("6");
+    } else if (key.code === "Digit7"|| key.code === "Numpad7") {
+        selectInput("7");
+    } else if (key.code === "Digit8"|| key.code === "Numpad8") {
+        selectInput("8");
+    } else if (key.code === "Digit9"|| key.code === "Numpad9") {
+        selectInput("9");
+    } else if (key.code === "NumpadMultiply") {
+        selectInput("*");
+    } else if ( key.code === "NumpadDivide") {
+        selectInput("/");
+    } else if ( key.code === "NumpadAdd") {
+        selectInput("+");
+    } else if ( key.code === "NumpadSubtract") {
+        selectInput("-");
+    } else if ( key.code === "NumpadEnter" || key.code === "Enter" ) {
+        equals();
+    } else if (key.code === "Backspace") {
+        deleteInput();
+    } else {
+        console.log("not a valid input")
+    }
+})
+
+
 equalsBtn.addEventListener("click", function() {equals()});
 cancelBtn.addEventListener("click", function() {clearDisplay()});
 deleteBtn.addEventListener("click", function() {deleteInput()});
 
 function selectInput(choice) {
+  // audio.pause()
+  // audio.play()
     let calciDisplay = document.getElementById("display");
     displayValue = calciDisplay.value + choice;
     calciDisplay.setAttribute("value", displayValue);
